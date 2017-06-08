ee2709d92a939252af721ae050936aece762ca56
Jason Rudy <jcrudy@gmail.com>
2017-06-08 14:43:43 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+import versioneer
+
+
+setup(name='modulecache',
+      version=versioneer.get_version(),
+      cmdclass=versioneer.get_cmdclass(),
+      description='Tools for caching modules for speedier imports',
+      author='Jason Rudy',
+      author_email='jcrudy@gmail.com',
+      url='https://github.com/jcrudy/modulecache',
+      packages=find_packages(),
+      install_requires=[],
+      tests_require=[]
+     )
