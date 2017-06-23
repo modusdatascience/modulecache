759d87f8730cc29f805e17f8aa9b83292c449daf
Jason Rudy <jcrudy@gmail.com>
2017-06-23 11:01:56 -0700

    Add logical operations to invalidators and use ascii encoding for FileChangeInvalidator

--- modulecache/invalidators.py
+++ modulecache/invalidators.py
@@ -1,6 +1,8 @@
 from abc import ABCMeta, abstractmethod
 from .base import ModuleCacheValid
 from modulecache.base import nocache, path_of_caller
+from toolz.functoolz import flip
+import inspect
 
 class ModuleCacheInvalidator(object):
     __metaclass__ = ABCMeta
@@ -29,6 +31,15 @@ class ModuleCacheInvalidator(object):
     def new_metadata(self, moduledata):
         pass
     
+    def __and__(self, other):
+        return AllInvalidator(self, other)
+    
+    def __or__(self, other):
+        return AnyInvalidator(self, other)
+    
+    def __invert__(self):
+        return NotInvalidator(self)
+    
 #     def _check(self, metadata, moduledata):
 #         try:
 #             mymeta = metadata[self.identity]
@@ -36,6 +47,78 @@ class ModuleCacheInvalidator(object):
 #             raise ModuleCacheInvalid()
 #         self._check(self, mymeta, moduledata)
 
+
+class DerivedInvalidator(ModuleCacheInvalidator):
+    pass
+    
+class MultipleDerivedInvalidator(DerivedInvalidator):
+    def __init__(self, *invalidators):
+        self.invalidators = invalidators
+        
+    def new_metadata(self, moduledata):
+        return tuple([inv.new_metadata(moduledata) for inv in self.invalidators])
+    
+    def _member_check(self, invalidator, metadata, moduledata):
+        '''Return True for valid and False for invalid'''
+        try:
+            invalidator.check(metadata, moduledata)
+            result = False
+        except ModuleCacheValid:
+            result = True
+        return result
+    
+    def _member_checks(self, metadata, moduledata):
+        return tuple(map(flip(self._member_check)(moduledata), zip(metadata, self.invalidators)))
+
+class SingleDerivedInvalidator(DerivedInvalidator):
+    def __init__(self, invalidator):
+        self.invalidator = invalidator
+    
+    def new_metadata(self, moduledata):
+        return self.invalidator.new_metadata(moduledata)
+    
+class AllInvalidator(MultipleDerivedInvalidator):
+    '''
+    Cache is valid if and only if all members say it is valid.
+    '''
+    def _check(self, metadata, moduledata):
+        if all(self._member_checks(metadata, moduledata)):
+            raise ModuleCacheValid
+    
+    def __and__(self, other):
+        if isinstance(other, AllInvalidator):
+            return AllInvalidator(self.invalidators + other.invalidators)
+        else:
+            return NotImplemented
+
+class AnyInvalidator(MultipleDerivedInvalidator):
+    '''
+    Cache is valid if and only if any member says it is valid.
+    '''
+    def _check(self, metadata, moduledata):
+        if any(self._member_checks(metadata, moduledata)):
+            raise ModuleCacheValid
+    
+    def __or__(self, other):
+        if isinstance(other, AnyInvalidator):
+            return AnyInvalidator(self.invalidators + other.invalidators)
+        else:
+            return NotImplemented
+    
+class NotInvalidator(SingleDerivedInvalidator):
+    '''
+    Cache is valid if and only if member says it is not valid.
+    '''
+    def _check(self, metadata, moduledata):
+        try:
+            self.invalidator._check(metadata, moduledata)
+            raise ModuleCacheValid()
+        except ModuleCacheValid:
+            pass
+    
+    def __invert__(self):
+        return self.invalidator
+    
 class AlwaysValid(ModuleCacheInvalidator):
     def _check(self, metadata, moduledata):
         raise ModuleCacheValid()
@@ -58,7 +141,13 @@ class VersioneerInvalidator(ModuleCacheInvalidator):
     def new_metadata(self, moduledata):
         return self.versioneer_info
 
-class FileChangeInvalidator(ModuleCacheInvalidator):
+class ChangeInvalidator(ModuleCacheInvalidator):
+    def _check(self, metadata, moduledata):
+        contents = self.new_metadata(moduledata)
+        if metadata == contents:
+            raise ModuleCacheValid()
+        
+class FileChangeInvalidator(ChangeInvalidator):
     '''
     Invalidate the cache if the specified file has changed.  Stores the entire file as metadata.
     '''
@@ -66,16 +155,7 @@ class FileChangeInvalidator(ModuleCacheInvalidator):
         ModuleCacheInvalidator.__init__(self, backend)
         self.path = path #if path is not None else path_of_caller()
         
-    def _check(self, metadata, moduledata):
-        contents = self.new_metadata(moduledata)
-        if metadata == contents:
-            raise ModuleCacheValid()
-    
     def new_metadata(self, moduledata):
         with open(self.path, 'rb') as infile:
-            contents = infile.read()
+            contents = infile.read().encode('utf8')
         return contents
-    
-    
-        
-        
